40e39efd0c47d3991d4b9773368939bae28b58e7
Josh Williams <spankyj@gmail.com>
2014-06-28 08:54:44 -0700

    Adding ability to F5 from VS project to run tests against live source tree

---
+++ projects/vs2013/UnitTestApp/UnitTestApp.Shared/js/default.js
@@ -0,0 +1,18 @@
+﻿// For an introduction to the Blank template, see the following documentation:
+// http://go.microsoft.com/fwlink/?LinkID=392286
+(function () {
+
+    // @TODO: get less working so we can load CSS from tree
+
+    // @TODO: get tests working so we can run tests from tree
+
+    // @TODO: figure out how to tell files are edited so that we can only list tests that depend on them
+    //  - note that this will likely need to be smart and take into account the directory in 
+    //    which private modules live b/c we won't want the actual dependency walk or else editing
+    //    promises means you have to test the world
+
+    // @TODO: add a nice UI with flyouts and ListViews and such to select which tests to run
+
+    // @TODO: the actual tests being run however are just a QUnit page in a x-ms-webview, right?
+
+})();
---
+++ projects/vs2013/UnitTestApp/UnitTestApp.Shared/winjs.js
@@ -0,0 +1,21 @@
+﻿
+if (!window.WinJS) {
+
+    require.config({
+        paths: {
+            "WinJS": "/content/winjs/js/WinJS",
+            "require-style": "/require-style",
+        },
+    });
+
+    window.WinJS = window.WinJS || {};
+    WinJS.Utilities = WinJS.Utilities || {};
+    WinJS.Utilities._writeProfilerMark = function _writeProfilerMark(text) {
+        window.msWriteProfilerMark && msWriteProfilerMark(text);
+    };
+
+    require(["/content/winjs/js/base.js", "/content/winjs/js/ui.js"], function (base, ui) {
+        // loaded
+    });
+
+}
